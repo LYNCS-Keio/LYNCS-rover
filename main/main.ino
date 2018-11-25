471e3d3b2986da216b7a05e14e68828dca0d313e
naoki-cpp <puni87gou@gmail.com>
2018-11-25 13:21:03 +0900

    使われていない定義を削除

--- main/main.ino
+++ main/main.ino
@@ -5,19 +5,8 @@
 #include "MPU6050_6Axis_MotionApps20.h"
 #include "local_libs/RoverMotor.h"
 
-#define bv 0.05745024
-#define cv 0.009521774
-#define A_M 0.0017
-#define A_m 0.0013
-#define B_M 0.53
-#define B_m 0.5996
-#define u 0
-#define PI 3.1415
 #define echoPin 13 // Echo Pin
 #define trigPin 7  // Trigger Pin
-#define anga 1
-#define angb 1
-#define MaxP 1
 #define MaxC 1 // per sec
 #define MaxA 1
 
