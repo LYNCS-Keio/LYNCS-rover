146c97035d5723422e1422338ca8b446595ccdf0
Naoki Yano <puni87gou@gmail.com>
2019-01-16 03:08:54 +0900

    .travis.yml等の変更

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -3,7 +3,6 @@ echo "Downloading version $ARD_VER of the Arduino IDE..."
 wget http://downloads.arduino.cc/arduino-$ARD_VER-linux64.tar.xz
 tar xf arduino-$ARD_VER-linux64.tar.xz
 git clone https://github.com/jrowberg/i2cdevlib.git
-sudo mv i2cdevlib/Arduino/* arduino-$ARD_VER/libraries
+mv i2cdevlib/Arduino/* arduino-$ARD_VER/libraries
 sudo mv arduino-$ARD_VER /usr/local/share/arduino
 sudo ln -s /usr/local/share/arduino/arduino /usr/local/bin/arduino
-arduino --install-library "Adafruit SleepyDog Library,Adafruit MQTT Library"
