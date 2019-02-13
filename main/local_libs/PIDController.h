efbbd682aac100364b828764afb5a4daa9b219c5
Naoki Yano <puni87gou@gmail.com>
2019-02-13 18:36:26 +0900

    Set関数を追加

--- main/local_libs/PIDController.h
+++ main/local_libs/PIDController.h
@@ -1,21 +1,32 @@
 #pragma once
 namespace lyncs
 {
- class PIDController
+class PIDController
 {
   private:
-	const double kProportionGain_;
-	const double kIntegralGain_;
-	const double kDifferentialGain_;
- 	double integral_;
+	double kProportionGain_;
+	double kIntegralGain_;
+	double kDifferentialGain_;
+	double integral_;
 	double prev_error_;
 	double manipulative_variable_;
- 	PIDController();
+	PIDController();
+
   public:
 	PIDController(const double kProportionGain, const double kIntegralGain, const double kDifferentialGain);
 	~PIDController();
 	void InputPID(double data, double goal, double dt);
 	const double GetPID() const;
+	[[deprecated("Propotion gain must not be changed.")]] void SetPropotionGain(double propotion_gain) {
+		kProportionGain_ = propotion_gain;
+	}
+		[[deprecated("Integral gain must not be changed.")]] void SetIntegralGain(double integral_gain)
+	{
+		kIntegralGain_ = integral_gain;
+	}
+	[[deprecated("Differential gain must not be changed.")]] void SetDifferentialGain(double differential_gain) {
+		kDifferentialGain_ = differential_gain;
+	}
 };
- 
- } // namespace lyncs 
+
+} // namespace lyncs
