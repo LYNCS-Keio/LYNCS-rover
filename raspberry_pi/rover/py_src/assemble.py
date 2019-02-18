39c49ed3ba1a5e1b9e998d3db1d85c82fd79c650
Naoki Yano <puni87gou@gmail.com>
2019-02-18 18:22:43 +0900

    Update assemble.py

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -4,12 +4,13 @@ from time import sleep
 
 goal_lat = 0
 goal_long =0
-
+cs = lyncs_rover.arduino_control()
+cs.Init()
 while True:
-    lyncs_rover.Csearch1()
+    cs.Csearch1()
     r_theata=gps.r_theta_to_goal(goal_lat, goal_long)
     if r_theata[0] < 20:
-        lyncs_rover.Csearch2()
+        cs.Csearch2()
     else:
         lyncs_rover.TransferValuesToArduino((int)(r_theata[0]/1000), 0)
     sleep(2)
