508c216c896911fefd5d15e3f5f4b6a3ba6d132a
Naoki Yano <puni87gou@gmail.com>
2019-02-20 15:46:36 +0900

    bme280

---
+++ raspberry_pi/bme280/sannshou_jikken.py
@@ -0,0 +1,12 @@
+class Data_manipulate:
+    def __init__(self,first_data):
+        self.judge_pathH_data = first_data
+
+def judgeHight2(path_hight):
+
+	path_hight = 2
+
+if __name__ == '__main__':
+    D_m = Data_manipulate(0)
+    judgeHight2(D_m.judge_pathH_data)
+    print(D_m.judge_pathH_data)
