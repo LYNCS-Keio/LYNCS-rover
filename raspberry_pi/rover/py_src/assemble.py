d9cf440df6245e679f9c140b6970db8c27d6a7e9
Naoki Yano <puni87gou@gmail.com>
2019-02-23 16:08:18 +0900

    Update assemble.py

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -6,7 +6,6 @@ if cs.Init() == -1:
     print('error')
     exit()
 while True:
-<<<<<<< HEAD
     length, theta = gps.r_theta_to_goal(35.555225, 139.654664)
     for i in range(25):
         judge1=cs.Csearch1()
@@ -17,12 +16,3 @@ while True:
     #else:
     if length*1000=>20:
         cs.Transfer(int(theta*1000), 5)
-=======
-    cs.Csearch1()
-    r_theata = gps.r_theta_to_goal(goal_lat, goal_long)
-    if r_theata[0]*1000 < 20:
-        cs.Csearch2()
-    else:
-        cs.Transfer((int)(r_theata[0]*1000), 0)
-    sleep(1)
->>>>>>> b707e51e5f00c81c1fe8cec9d4e225282493a592
