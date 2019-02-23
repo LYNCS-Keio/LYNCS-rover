d06b3dc3f2405b66ecfb2502641f7d2fa7b288bf
Naoki Yano <puni87gou@gmail.com>
2019-02-23 16:06:31 +0900

    Update ArduinoControl.h

--- raspberry_pi/rover/include/ArduinoControl.h
+++ raspberry_pi/rover/include/ArduinoControl.h
@@ -10,11 +10,7 @@ class ArduinoControl
 	ArduinoControl(/* args */);
 	~ArduinoControl();
 	int Init();
-<<<<<<< HEAD
-	int Csearch1();
-=======
 	int Transfer(int angle, unsigned char order);
 	void Csearch1();
->>>>>>> b707e51e5f00c81c1fe8cec9d4e225282493a592
 	void Csearch2();
 };
