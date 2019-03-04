d574c1153d701479eaf77c1c596c01610998fa50
Naoki Yano <puni87gou@gmail.com>
2019-03-04 11:18:26 +0900

    test用

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -8,56 +8,48 @@ goal_lat, goal_log = [35.555744, 139.654071]
 cs = lyncs_rover.arduino_control()
 if cs.Init() == -1:
     print('error')
+## test before falling
+count = 0
+while True:
+    count += 1
+    judge_data0 = height.readData()
+    cs.LogOutput('phase1, height::' + str(judge_data0))
+    if count > 20:
+        break
+
+while True:
+    coord = gps.lat_long_measurement()
+    if coord is not None:
+        cs.LogOutput('lat::' + str(coord[0]) + ', long::' + str(coord[1]))
+        break
+
+sleep(30)  # fall
+
+# after landing
+cs.LogOutput('landed.')
 
 count = 0
 while True:
     count += 1
     judge_data0 = height.readData()
-    if count % 10 == 1:
-        cs.LogOutput('phase1, height::' + str(judge_data0))
-    if judge_data0 > height.max_high:
+    cs.LogOutput('phase2, height::' + str(judge_data0))
+    if count > 20:
         break
 
 while True:
-    count += 1
-    judge_data = height.readData()
-    if count % 10 == 1:
-        cs.LogOutput('phase2, height::' + str(judge_data))
-
-    if judge_data < height.low_high and height.math.fabs(height.given_data -
-                                                         judge_data) < 0.8:
-        break
-    height.given_data = judge_data
-
-cs.LogOutput('landed.')
-cs.Transfer(0,6)
-sleep(5)
-
-length, theta = [0,0]
-while True:
-    list_dis_thet = gps.r_theta_to_goal(goal_lat, goal_log)
-    if list_dis_thet is not None:
-        length, theta = list_dis_thet
-        break
-
-while True:
-
-    list_dis_thet = gps.r_theta_to_goal(goal_lat, goal_log)
-    if list_dis_thet is not None:
-        length, theta = list_dis_thet
-
     coord = gps.lat_long_measurement()
     if coord is not None:
         cs.LogOutput('lat::' + str(coord[0]) + ', long::' + str(coord[1]))
-    length_theta = gps.r_theta_to_goal(goal_lat, goal_log)
-    if length_theta is not None:
-        cs.LogOutput('dist::' + str(length) + ', angle::' + str(theta))
+        break
+
+cs.Transfer(0,2)
+sleep(20)
+
+while True:
+
     for i in range(25):
         judge = cs.Csearch1()
-        if length * 1000 < 5 and judge == 1:
+        if judge == 1:
             cs.Csearch2()
-    # f r_theata[0]*1000 < 20:
-    #    cs.Csearch2()
-    # else:
-    if length * 1000 >= 5:
-        cs.Transfer(int(theta * 1000), 5)
+
+cs.Transfer(0, 3)
