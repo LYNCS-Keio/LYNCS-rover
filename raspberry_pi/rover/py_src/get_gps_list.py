ebae87284e60ceccaaba6c64be21e56f08b215fc
Naoki Yano <puni87gou@gmail.com>
2019-03-04 21:56:24 +0900

    速度の追加

--- raspberry_pi/rover/py_src/get_gps_list.py
+++ raspberry_pi/rover/py_src/get_gps_list.py
@@ -5,16 +5,32 @@ import rover_module as gps
 import time
 import sys
 args = sys.argv
+"""
+Usage
+-------
+例えばgpsのデータが100個欲しい場合, 
+```bash
+python3 get_gps_list.py 100
+```
+を実行するとgpsのデータが100個入ったcsvファイルが作成されます。
+"""
 
-data_num = int(args[1])
+data_num = 10  #int(args[1])
 data_path = 'gps_data.csv'
 with open(data_path, mode='w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow([data_num])
+    writer.writerow(['緯度', '経度', '速さ', '方位角', '総数', data_num])
     for i in range(data_num):
         lat_long = gps.lat_long_measurement()
-        if lat_long[0] is not None and lat_long[1] is not None:
-            writer.writerow([time.time(), lat_long[0], lat_long[1]])
+        vel = gps.velocity_measurement()
+        if lat_long[0] is not None and lat_long[1] is not None and vel[
+                0] is not None and vel[1] is not None:
+            writer.writerow([
+                time.time(), lat_long[0], lat_long[1], vel[0] * 1852 / 3600,
+                vel[1]
+            ])
+# knot から mへの換算
+# 角度の
 
 with open(data_path) as f:
     print(f.read())
