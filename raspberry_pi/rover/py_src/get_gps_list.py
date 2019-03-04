7f5a5b333eda3798efd8d05979aecbf6d5067820
Naoki Yano <puni87gou@gmail.com>
2019-03-04 21:08:03 +0900

    get_gps_list.pyの作成

---
+++ raspberry_pi/rover/py_src/get_gps_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#%%
+import csv
+import rover_module as gps
+import time
+
+data_num = 5
+data_path = 'gps_data.csv'
+with open(data_path, mode='w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow([data_num])
+    for i in range(data_num):
+        lat_long = gps.lat_long_measurement()
+        if lat_long[0] is not None and lat_long[1] is not None:
+            writer.writerow([time.time(), lat_long[0], lat_long[1]])
+
+with open(data_path) as f:
+    print(f.read())
+# 0,1,2
+# "a,b,c","x","y"
