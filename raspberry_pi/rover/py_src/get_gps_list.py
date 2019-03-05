b7aa09c5dbbf520355d2cf8b80bb50d0c89dcf3d
Naoki Yano <puni87gou@gmail.com>
2019-03-05 12:49:45 +0900

    時間のヘッダを追加

--- raspberry_pi/rover/py_src/get_gps_list.py
+++ raspberry_pi/rover/py_src/get_gps_list.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #%%
 import csv
-import rover_module as gps
+#import rover_module as gps
+import  random
 import time
 import sys
 args = sys.argv
@@ -15,11 +16,12 @@ python3 get_gps_list.py 100
 を実行するとgpsのデータが100個入ったcsvファイルが作成されます。
 """
 
+
 data_num = int(args[1])
 data_path = 'gps_data.csv'
 with open(data_path, mode='w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['緯度', '経度', '速さ', '方位角', '総数', data_num])
+    writer.writerow(['時間', '緯度', '経度', '速さ', '方位角', '総数', data_num])
     for i in range(data_num):
         lat_long = gps.lat_long_measurement()
         vel = gps.velocity_measurement()
@@ -29,6 +31,7 @@ with open(data_path, mode='w', newline='') as f:
                 time.time(), lat_long[0], lat_long[1], vel[0] * 1852 / 3600,
                 vel[1]
             ])
+        time.sleep(0.5)
 # knot から mへの換算
 # 角度の単位は度
 
